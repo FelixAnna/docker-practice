93917aa2afb9b628a0cb0649a4764496fecb7463
FelixAnna <123456a>
2019-07-28 12:39:20 +0800

    Initial Commit

---
+++ Docker.Demo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Docker.Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Docker,Hello World!");
+        }
+    }
+}
